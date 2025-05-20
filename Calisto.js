9c9f6176817024fdd874adacc9a27e8e99ec97b4
Germanius <german.lippert@mail.ru>
2025-05-20 04:31:10 -0700

    Added glimmering stars and a archetype for a sign. In the future should add more cooler-looking stars and improve the sign

--- Calisto.js
+++ Calisto.js
@@ -2,12 +2,13 @@
 // EFFECTS: returns an array of shadow-boxes at random possitions
 function giveStars(n) {
     let shadows = [];
-    for (let i = 1; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         const x = Math.floor(Math.random() * 2000);
         const y = Math.floor(Math.random() * 2000);
         shadows.push(`${x}px ${y}px #FFF`);
     }
-
     return shadows.join(',');                // need to return with , between each due to css syntax
 }
 
+document.getElementById('stars').style.boxShadow = giveStars(600);
+document.getElementById('stars2').style.boxShadow = giveStars(400);
